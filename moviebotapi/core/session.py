eed5d9b90c1b9800af76ba9ce4d8680326193f86
yee <pofey@me.com>
2023-01-11 11:01:41 +0800

    new version

--- moviebotapi/core/session.py
+++ moviebotapi/core/session.py
@@ -6,7 +6,7 @@ from httpx import Timeout
 
 from moviebotapi.core.exceptions import ApiErrorException, NetworkErrorException, IllegalAuthorization
 
-UA = 'moviebotapi/0.0.44'
+UA = 'moviebotapi/0.0.45'
 URLTypes = Union["URL", str]
 HeaderTypes = Union[
     "Headers",

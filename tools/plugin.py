9e10a99eb4bad6372792a8ecf4ceec200f2ddb9f
pofey@me.com <pofey@me.com>
2023-02-08 09:53:14 +0800

    add changelog

--- tools/plugin.py
+++ tools/plugin.py
@@ -1,4 +1,5 @@
 import os
+import urllib
 
 import httpx
 from httpx import Timeout
@@ -39,7 +40,7 @@ def get_ak(email, password):
     return None
 
 
-def publish(ak, plugin_zip_path):
+def publish(ak, plugin_zip_path, change_log):
     if not os.path.exists(plugin_zip_path):
         print(f'插件压缩包路径不存在：{plugin_zip_path}')
         return
@@ -48,7 +49,7 @@ def publish(ak, plugin_zip_path):
         return
     files = {'file': (os.path.split(plugin_zip_path)[-1], open(plugin_zip_path, 'rb'), 'application/zip')}
     r = httpx.post(
-        f'{REPO}/api/plugins/publish',
+        f'{REPO}/api/plugins/publish?changeLog={urllib.parse.quote_plus(change_log)}',
         files=files,
         headers={
             'token': ak,
@@ -60,5 +61,10 @@ def publish(ak, plugin_zip_path):
 
 
 if __name__ == '__main__':
-    ak = get_ak('xxx', 'xxx')
-    publish(ak, '/xxx.zip')
+    ak = get_ak('xx', 'xx')
+    publish(
+        ak,
+        '/Users/yee/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/f4ba1fd4852be9ffffae3a6d5d7065f3/Message/MessageTemp/4dce4cee3ce4cfdc18a754c065cb9014/File/site_signin.zip',
+        '''修复部分站点签到失败的bug
+        开放定时签到时间设置'''
+    )
